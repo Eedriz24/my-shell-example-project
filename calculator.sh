b21c99283e514ddc80edf4222e4ac3dff3cacc70
Ubuntu <ubuntu@ip-172-31-43-92.ec2.internal>
2025-08-22 22:34:37 +0000

    system tracking

--- calculator.sh
+++ calculator.sh
@@ -1,3 +1,26 @@
-a+b =c 
-b-c = d
-y = p-q
+
+#!/bin/bash
+###########
+#Author: Opeyemi Morakinyo
+#Date : 23-08-2025
+#Version : V1
+#################
+#This script is for tracking system
+
+set -x
+set -o pipefail
+
+#List of S3 bucket
+aws s3 ls
+
+#List of Ec2 instances
+aws ec2 describe-instances
+
+#List of lambda functions
+aws lambda list-functions
+
+#List IAM Users
+
+aws IAM list-users
+
+a+b =c
